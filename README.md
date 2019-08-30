dd9a073081f0c0a062f014c3fdde2480c572b01a
罗成海 <1553650095@qq.com>
2019-08-30 19:39:43 +0800

    init  my project

---
+++ README.md
@@ -0,0 +1,2 @@
+#这是一个移动端的基于vue的项目
+##主流开源协议之间的有何异同？（https://blog.csdn.net/xiaodaima2016/article/details/83645854）
---
+++ src/main.js
@@ -0,0 +1,21 @@
+// 入口文件
+import Vue from 'vue'
+
+//导入Mui 的css样式
+import './lib/mui/css/mui.min.css'
+    
+//按需导入 Mint-UI中的组件
+import { Header } from 'mint-ui'
+Vue.component(Header.name,Header)
+
+
+
+
+//导入App 根组件
+ import app from './App.vue'
+
+
+var vm = new Vue({
+    el: '#app',
+    render: c =>c(app)
+})
